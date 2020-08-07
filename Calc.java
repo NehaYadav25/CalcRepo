74a4691dd45e10a9649ac71720b73a3d27143c19
NehaYadav25 <48085744+NehaYadav25@users.noreply.github.com>
2020-08-07 17:29:15 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,19 +1,23 @@
-class Calc{
-   public void add(int a, int b){
-      System.out.println("Sum="+a+b);
-    }
-   public void diff(int a,int b){
-      System.out.println("Diff"+ (a-b));
-   }
+public class HelloWorld{
 
-   public void prod(int a, int b){
-       System.out.println("Product= "+(a*b));
-   }
-   public void div(int a,int b){
-      System.out.println("Quotient"+ (a/b));
+     public static void main(String []args){
+        add(5,7);
+            diff(3,6);
+     }
+     
+     
+      public static void add(int a, int b){
+         System.out.println("Sum="+a+b);
+       }
+      public static void diff(int a,int b){
+         System.out.println("Diff"+ (a-b));
+      }
 
-   }
-      
-      
-   
+      public static void prod(int a, int b){
+          System.out.println("Product= "+(a*b));
+      }
+      public static void div(int a,int b){
+         System.out.println("Quotient"+ (a/b));
+
+      }
 }
