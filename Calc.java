443fd6529e8ead20fbd2a695f51345ec40e9a271
NehaYadav25 <48085744+NehaYadav25@users.noreply.github.com>
2020-08-07 16:35:27 +0530

    Create Calc.java
    Created calc file.

---
+++ Calc.java
@@ -0,0 +1,22 @@
+class Calc{
+   public void add(int a, int b){
+      System.out.println("Sum="+a+b);
+    }
+   public void diff(int a,int b){
+      System.out.println("Diff"+ (a-b));
+   }
+
+   public void prod(int a, int b){
+       System.out.println("Product= "+(a*b));
+
+   public void div(int a,int b){
+      System.out.println("Quotient"+ (a/b));
+
+   }
+      
+      public void div(int a,int b){
+      System.out.println("Quotient"+ (a/b));
+      System.out.println("Quotient"+ (a/b));
+
+   }
+}
